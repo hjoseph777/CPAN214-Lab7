8b30ab4841067008e63f6c0cd6330becb37acd32
H Joseph <hjoseph777@gmail.com>
2025-11-10 10:06:43 -0500

    Fix static files serving configuration
    - Simplify STATIC_URL to use leading slash
    - Remove unnecessary STATICFILES_DIRS
    - Remove URL static files configuration (WhiteNoise handles this)
    - Fix CSS 404 errors by correcting static files setup
    
    CSS should now load properly at /static/main/style.css

--- harry_joseph/harry_joseph/urls.py
+++ harry_joseph/harry_joseph/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from main.views import home, greetings, now, favicon
 
 urlpatterns = [
@@ -10,4 +8,4 @@ urlpatterns = [
     path('greetings/', greetings, name='greetings'),
     path('now/', now, name='now'),
     path('favicon.ico', favicon, name='favicon'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
